7544bdb159fbb08105ec7a1b8d90de7cf9eab514
Kachii <kachikaduru@gmail.com>
2025-01-05 11:22:58 +0100

    Uploaded images to the Supabase bucket wrote a function that creates a cabin and adds the uplaoded image to the supabase bucket and it reflects on the app.

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -29,7 +29,7 @@ function CreateCabinForm() {
 
   function addNewCabin(data) {
     // console.log(data);
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -92,7 +92,11 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", { required: "This field is required" })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,5 +1,5 @@
 import { handleError } from "../utils/helpers";
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -10,9 +10,24 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]).select();
+  const imageName = `${newCabin.image.name}__${Math.random()}`.replaceAll("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
+  // 1. create a new cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }])
+    .select();
   handleError(error);
+
+  // 2. Uplaod the image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  if (storageError) await supabase.from("cabins").delete().eq("id", data.id);
+
+  return data;
 }
 
 export async function deleteCabin(id) {
